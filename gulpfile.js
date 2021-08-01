95b7f3df681722ad3c1d600484b4bdfa6112f44d
varvarakulikova <varvara.kulikova.98@gmail.com>
2021-08-02 00:43:28 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 const csso = require('gulp-csso');
 const include = require('gulp-file-include');
 const htmlmin = require('gulp-htmlmin');
+const imgmin = require('gulp-imagemin');
 const del = require('del');
 const concat = require('gulp-concat');
 const prefix = require('gulp-autoprefixer');
@@ -24,8 +25,14 @@ function scss() {
         .pipe(sass())
         .pipe(prefix())
         .pipe(csso())
-        .pipe(concat('index.css'))
-        .pipe(dest('dist'))
+        .pipe(concat('style.css'))
+        .pipe(dest('dist/css'))
+}
+
+function img() {
+    return src('src/img/*')
+        .pipe(imgmin())
+        .pipe(dest('dist/img'))
 }
 
 function clear() {
@@ -43,6 +50,7 @@ function serve() {
 
 exports.html = html
 exports.scss = scss
-exports.build = series(clear, scss, html)
-exports.serve = series(clear, scss, html, serve)
+exports.img = img
+exports.build = series(clear, scss, html, img)
+exports.serve = series(clear, scss, html, img, serve)
 exports.clear = clear
